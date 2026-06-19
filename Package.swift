06f63ebcca4940a58c0318c00f55382b0955a535
M Hegazy <156926080+MHegazy89@users.noreply.github.com>
2026-06-19 13:18:50 +0200

    Fix Package.swift: use macOS 14 for Swift 5.9 compatibility

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Textream",
-    platforms: [.macOS(.v15)],
+    platforms: [.macOS(.v14)],
     targets: [
         .executableTarget(
             name: "Textream",
